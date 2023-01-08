47de548cbb2d45e54da7dc2909f9b58149983ccb
Md Ashraful Ashiq <75539261+Md-Ashraful-Ashiq@users.noreply.github.com>
2023-01-08 21:38:00 +0800

    Update Cow.java

--- AnimalFarm/src/animalfarm/Cow.java
+++ AnimalFarm/src/animalfarm/Cow.java
@@ -6,12 +6,15 @@ import javax.swing.*;
  *
  * @author User
  */
-public class Cow extends Animal {
+public class Cow extends Decoration {
 
     public Cow() {
         name = "cow";
         image = new ImageIcon("res/cow.png");
-        audio = "moo";
-    }
+        // audio = "moo";
+        }
 
+    public ImageIcon getCow() {
+        return image;
+    }
 }
