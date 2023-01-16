bb195e395e668c17ad4e626837f9a8c580df3df4
munirrani <munirmohamadrani@gmail.com>
2023-01-16 14:23:14 +0800

    Update Animal class

--- AnimalFarm/src/animalfarm/Animal.java
+++ AnimalFarm/src/animalfarm/Animal.java
@@ -1,5 +1,7 @@
 package animalfarm;
 
+import java.awt.event.*;
+
 import javax.swing.*;
 
 /**
@@ -11,8 +13,14 @@ public abstract class Animal implements Element {
     String name;
     ImageIcon image;
     String audio;
+    int hunger_level = 30;
+    static int total_animal_count = 0;
+    int animal_index = 0;
+    int x; 
+    int y;
 
-    public Animal() {
+    public Animal() {    
+
     }
 
     @Override
@@ -41,6 +49,33 @@ public abstract class Animal implements Element {
 
     public void setAudio(String audio) {
         this.audio = audio;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void startTimer() {
+        animal_index = total_animal_count;
+        total_animal_count++;
+        int interval_in_seconds = 5;
+        Timer timer = new Timer(interval_in_seconds * 1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hunger_level -= 5;
+                System.out.println("Animal " + name + " " + animal_index + " hunger level: " + hunger_level);
+                if (hunger_level <= 0) {
+                    System.out.println("Animal " + name + " " + animal_index + " died of hunger (HOW DARE YOU?)");
+                    AnimalFarm.getFarmInstance().deleteElement(x, y);
+                    ((Timer) e.getSource()).stop();
+                }
+            }
+        });
+        timer.start();
     }
 
 }
